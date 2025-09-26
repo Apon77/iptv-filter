82392266c7af77e973adb60c4afe31c523484d60
Apon77 <khalakuzzamanapon5@gmail.com>
2025-09-27 01:32:05 +0600

    working with nice ui

--- src/script.js
+++ src/script.js
@@ -1,7 +1,10 @@
 var output = document.getElementById('output')
 var progress = document.getElementById('progress')
 var download = document.getElementById('download')
+var search = document.getElementById('search')
 var filename = ''
+
+search.style.display = 'none'
 
 async function readText(event) {
     const file = event.target.files.item(0)
@@ -20,10 +23,11 @@ async function readText(event) {
         }
     }
     progress.innerHTML = 'completed'
+    search.style.display = 'block'
 }
 
 function downloadfile() {
-    var text = output.innerHTML
+    var text = download.innerHTML
     var blob = new Blob([text], { type: 'text/plain' })
     var link = document.createElement('a')
     link.href = URL.createObjectURL(blob)
@@ -58,11 +62,33 @@ async function fetchdata(url, infourl) {
         if (response.ok) {
             data = await response.text()
             if (data.includes('#EXT')) {
-                output.innerHTML += infourl + '\n' + url + '\n' + '\n'
+                download.innerHTML += infourl + '\n' + url + '\n' + '\n'
+                channelname = infourl.split(',')[1]
+                chwithlink = `<li><a href=${url}>${channelname}</a></li>`
+                output.innerHTML += chwithlink
+
             }
         } else { throw new Error('failed to fetch tv') }
     } catch (error) {
         console.log('error;', error)
     }
 
+}
+
+
+function myFunction() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("search");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("output");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
 }
